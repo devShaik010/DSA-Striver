be45ef11f3feea23c10aaa26bd6c862ebe4534fc
devShaik010 <160422747305@mjcollege.ac.in>
2025-03-22 22:04:26 +0530

    types is created

---
+++ Data-types/Number.java
@@ -0,0 +1,11 @@
+
+public class Number {
+   public static void main(String[] args) {
+       int n1 = 999999999;
+       short n2 = 9999;
+       long n3 =999999999999999999L;
+       System.out.println(n1);
+       System.out.println(n2);      
+       System.out.println(n3);
+   }    
+}
